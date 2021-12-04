1bc8ccca6eccf711fd708247913214d8e1e0d54c
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-12-04 22:23:19 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/jc_charactercountlimittf/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_charactercountlimittf/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.rujirakongsomran.jc_charactercountlimittf
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.rujirakongsomran.jc_charactercountlimittf.ui.theme.JC_CharacterCountLimitTFTheme
 
@@ -15,24 +22,50 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JC_CharacterCountLimitTFTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyComposable()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyComposable() {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var name by remember { mutableStateOf("") }
+        val maxChar = 10
+        OutlinedTextField(
+            value = name,
+            onValueChange = {
+                if (it.length < maxChar) {
+                    Toast.makeText(
+                        context,
+                        "it: $it",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    name = it
+                }
+            },
+            label = {
+                Text(text = "Name")
+            },
+            placeholder = {
+                Text(text = "Enter your name")
+            },
+            maxLines = 1
+        )
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun MyComposablePreview() {
     JC_CharacterCountLimitTFTheme {
-        Greeting("Android")
+        MyComposable()
     }
 }
